75393f7402196280c6a895651fc08b5406da8c2b
axiangcoding <axiangcoding@gmail.com>
2023-03-28 21:21:07 +0800

    remove unused project server

--- server/axbot-dao/src/main/java/com/github/axiangcoding/axbot/cache/CacheKeyGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.github.axiangcoding.axbot.cache;
-
-public class CacheKeyGenerator {
-    private static final String GAMER_PROFILE_UPDATE_CACHE_PREFIX = "WTGamerProfileUpdate";
-
-    public static String getWtGamerProfileUpdateCacheKey(String nickname) {
-        return "%s:%s".formatted(GAMER_PROFILE_UPDATE_CACHE_PREFIX, nickname);
-    }
-}
