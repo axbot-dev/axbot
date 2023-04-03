53ea9039125fba32f64dc1a10d8a396a43a375c3
axiangcoding <axiangcoding@gmail.com>
2023-04-04 01:13:50 +0800

    add java target compatibility

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 group = "com.github.axiangcoding.axbot"
 version = "1.0-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
+java.targetCompatibility = JavaVersion.VERSION_17
 
 
 allprojects {
