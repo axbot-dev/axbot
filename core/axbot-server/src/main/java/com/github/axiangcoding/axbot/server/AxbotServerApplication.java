a5621e879b69f5f6a22947ee80dca1412c2fd4a4
axiangcoding <axiangcoding@gmail.com>
2023-04-18 16:27:22 +0800

    Revert "Printing uncaught errors in all thread"
    This reverts commit 5f301feb12f1c3d957da8682eb2332cc4df976a4.

--- core/axbot-server/src/main/java/com/github/axiangcoding/axbot/server/AxbotServerApplication.java
+++ core/axbot-server/src/main/java/com/github/axiangcoding/axbot/server/AxbotServerApplication.java
@@ -1,7 +1,6 @@
 package com.github.axiangcoding.axbot.server;
 
 import com.github.axiangcoding.axbot.server.configuration.props.BotConfProps;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
@@ -10,15 +9,9 @@ import org.springframework.context.annotation.AnnotationBeanNameGenerator;
 
 @SpringBootApplication(nameGenerator = AnnotationBeanNameGenerator.class, exclude = {SecurityAutoConfiguration.class})
 @EnableConfigurationProperties(BotConfProps.class)
-@Slf4j
 public class AxbotServerApplication {
 
-    public static void init() {
-        Thread.setDefaultUncaughtExceptionHandler((t, e) -> log.error("thread throw uncaught exception", e));
-    }
-
     public static void main(String[] args) {
-        init();
         SpringApplication.run(AxbotServerApplication.class, args);
     }
 
