6da8a8b398c2aef98e18aacaae594bf5e4154eeb
yuxiang_wu <yuxiang_wu@foxitsoftware.com>
2023-05-12 09:38:13 +0800

    Update luck description

--- core/axbot-server/src/main/java/com/github/axiangcoding/axbot/server/service/axbot/function/LuckyFunction.java
+++ core/axbot-server/src/main/java/com/github/axiangcoding/axbot/server/service/axbot/function/LuckyFunction.java
@@ -26,15 +26,15 @@ public class LuckyFunction {
 
     private static String genExtra(int luck) {
         if (luck == 0) {
-            return "好一个0！";
+            return "我测，好一个0！";
         } else if (luck < 30) {
-            return "您就是非洲酋长失散多年的异父异母的亲兄弟";
+            return "玩战雷玩的";
         } else if (luck < 60) {
-            return "凑合凑合日子还得过";
+            return "哦我的上帝，战雷怎么你了";
         } else if (luck < 80) {
-            return "老欧洲人了，那叫一个地~道";
+            return "老白金人了，那叫一个地~道";
         } else if (luck < 100) {
-            return "来一发狠狠奖励自己！";
+            return "狠狠奖励奖励奖励奖励奖励奖励自己！";
         } else if (luck == 100) {
             return "到达最高分，理塘！";
         } else {
