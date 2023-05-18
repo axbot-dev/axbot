9c1f8306d85b213d36d15e88aba691aacb548398
axiangcoding <axiangcoding@gmail.com>
2023-05-19 00:58:22 +0800

    Delete unused test file

--- core/axbot-server/src/main/java/com/github/axiangcoding/axbot/server/AxbotServerApplication.java
+++ core/axbot-server/src/main/java/com/github/axiangcoding/axbot/server/AxbotServerApplication.java
@@ -1,10 +1,8 @@
 package com.github.axiangcoding.axbot.server;
 
-import com.github.axiangcoding.axbot.server.configuration.props.BotConfProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.AnnotationBeanNameGenerator;
 
 @SpringBootApplication(nameGenerator = AnnotationBeanNameGenerator.class, exclude = {SecurityAutoConfiguration.class})
