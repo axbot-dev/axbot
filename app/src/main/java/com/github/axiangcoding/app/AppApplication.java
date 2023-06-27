3bac24d790b926b8894d8a8c528185b0d864b5ea
axiangcoding <axiangcoding@gmail.com>
2023-06-27 09:09:29 +0800

    Fix codeql issues

--- app/src/main/java/com/github/axiangcoding/app/AppApplication.java
+++ app/src/main/java/com/github/axiangcoding/app/AppApplication.java
@@ -3,7 +3,6 @@ package com.github.axiangcoding.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.AnnotationBeanNameGenerator;
 
 @SpringBootApplication(
         // nameGenerator = AnnotationBeanNameGenerator.class,
