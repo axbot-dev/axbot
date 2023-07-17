5f1f8d803b985568973bd9a75d38b5febe4e32a6
axiangcoding <axiangcoding@gmail.com>
2023-07-17 09:39:26 +0800

    feat(app): 设置定时任务总开关

--- app/src/main/java/com/github/axiangcoding/axbot/app/server/configuration/SchedulingConfiguration.java
+++ app/src/main/java/com/github/axiangcoding/axbot/app/server/configuration/SchedulingConfiguration.java
@@ -3,11 +3,16 @@ package com.github.axiangcoding.axbot.app.server.configuration;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConditionalOnProperty(
+        value = "app.scheduling.enabled",
+        havingValue = "true",
+        matchIfMissing = true)
 @Configuration
 @EnableScheduling
 @EnableSchedulerLock(defaultLockAtMostFor = "10m", defaultLockAtLeastFor = "PT10S")
