5f80337eb407d2d46aa384fa93dbdbc3fd19c713
axiangcoding <axiangcoding@gmail.com>
2023-08-29 18:27:55 +0800

    fix(app): 修改响应的url地址
    Closes #228

--- app/src/main/java/com/github/axiangcoding/axbot/app/bot/function/passive/sys/FuncGetHelp.java
+++ app/src/main/java/com/github/axiangcoding/axbot/app/bot/function/passive/sys/FuncGetHelp.java
@@ -9,15 +9,17 @@ import love.forte.simbot.event.ChannelMessageEvent;
 
 @AxPassiveFunc(command = FunctionType.GET_HELP)
 public class FuncGetHelp extends AbstractPassiveFunction {
-    private static final String DOC_URL = "https://www.yuque.com/axiangcoding/ei27mo/omy4cgwvsikrwue1";
+    private static final String DOC_URL = "https://axbot-dev.github.io/axbot-doc/docs/document/user-manual";
     private static final String WEBSITE_URL = "https://axbot-dev.github.io/axbot-doc/";
+    private static final String AUTHOR_URL = "https://space.bilibili.com/8696650";
 
     @Override
     public void processForKOOK(ChannelMessageEvent event) {
         KOOKCardTemplate ct = new KOOKCardTemplate("获取帮助", "success");
         ct.addModuleContentSection("需要用@来调用v2的AXBot哦");
         ct.addModuleTextLink("查看项目主页", "项目主页", "primary", WEBSITE_URL);
-        ct.addModuleTextLink("快速用户手册", "用户手册", "primary", DOC_URL);
+        ct.addModuleTextLink("用户使用手册", "用户手册", "primary", DOC_URL);
+        ct.addModuleTextLink("开发者B站首页", "开发者首页", "primary", AUTHOR_URL);
         event.replyBlocking(toCardMessage(ct.displayWithFooter()));
     }
 
